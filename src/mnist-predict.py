e578b3ac32af170e4cb51f104ce527e947c89434
gotou0815 <gogogoto3104@gmail.com>
2023-07-10 18:28:23 +0900

    名前の変更

--- src/mnist-predict.py
+++ src/mnist-predict.py
@@ -9,11 +9,10 @@ import tensorflow as tf
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # 画像の保存
-img = X_train[0].reshape(28, 28)
-cv2.imwrite("MNIST_X_test[0].png", img)
+img = X_test[0].reshape(28, 28)
+cv2.imwrite("./image/MNIST_X_test[0].png", img)
 
 #データの成形。kerasは0.0~1.0までの、float型の配列じゃないと扱うことが出来ない
-X_train = X_train.reshape(60000, 784)/255
 X_test = X_test.reshape(10000, 784)/255
 
 # モデルを読み込む
@@ -21,7 +20,7 @@ model_path = "model/MNIST.h5"
 model = tf.keras.models.load_model(model_path)
 
 # Kerasではバッチ処理を前提としているため、データの型を(1,784)とする必要がある
-img_expand = X_train[0][np.newaxis, ...]
+img_expand = X_test[0][np.newaxis, ...]
 print(img_expand.shape)
 
 # X_test[0]をpredict
